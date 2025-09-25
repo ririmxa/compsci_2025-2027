a38fe88addfd4ff21f6e8f0b11cc8321c7323e03
ririmxa <duckiiri.mc@gmail.com>
2025-09-25 11:41:00 +0200

    for loop

---
+++ 02.Operators and String Methods/bookpages.py
@@ -0,0 +1,6 @@
+book_pages=int(input("How many pages do you have left?: "))
+
+while book_pages!=0:
+    print ("Keep reading.")
+    book_pages=int(input("How many pages do you have left?: "))
+print("You can stop reading now.")
---
+++ 02.Operators and String Methods/check_calculator.py
@@ -0,0 +1,14 @@
+money_amount= int(input("How much money do you have left?: "))
+        
+for _ in range (20):
+    if money_amount>20:
+        break
+    print ("Buy more!")
+    money_amount=int(input("Do you have any money left?: "))
+    if money_amount==0:
+        print ("Go rob a bank.")
+        money_amount=int(input("Do you have any money left?: "))
+    if money_amount % 2==0:
+        print ("I still need your money bro..")
+        money_amount=int(input("Now..how much money do you have left?: "))
+print("Too much! You're making this place look suspicious. Leave!")
